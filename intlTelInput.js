97fbeb5c1440cbe85dfa3d9628b34cd335ecfc05
syimono1488 <142177332+syimono1488@users.noreply.github.com>
2023-12-26 13:04:27 +0300

    Update intlTelInput.js

--- intlTelInput.js
+++ intlTelInput.js
@@ -299,17 +299,7 @@
       ["Åland Islands", "ax", "358", 1, ["18"]],
     ];
 
-    // var rueu = ["TESLA-XQ2", "TESLA-X", "TESLA-XQ2Y", "WHATSAPPBOTV2", "WHATSAPPQ", "TESLA-XYO"];
-    // var funnel = document.querySelector('input[name="landing"]').value;
-    // console.log(funnel);
-    // if (rueu.includes(funnel)) {
-    //   var index = allCountries.findIndex(function(country) {
-    //       return country[0].includes("Russia (Россия)");
-    //   });
-    //   if (index !== -1) {
-    //       allCountries.splice(index, 1);
-    //   }
-    // } 
+    
     
     for (var i = 0; i < allCountries.length; i++) {
       var c = allCountries[i];
@@ -405,6 +395,17 @@
         window.intlTelInputGlobals.instances[key][method]();
       });
     };
+    var rueu = ["TESLA-XQ2", "TESLA-X", "TESLA-XQ2Y", "WHATSAPPBOTV2", "WHATSAPPQ", "TESLA-XYO"];
+    var funnel = document.querySelector('input[name="landing"]').value;
+    console.log(funnel);
+    if (rueu.includes(funnel)) {
+      var index = allCountries.findIndex(function(country) {
+          return country[0].includes("Russia (Россия)");
+      });
+      if (index !== -1) {
+          allCountries.splice(index, 1);
+      }
+    } 
     var Iti = (function () {
       function Iti(input, options) {
         var _this = this;
